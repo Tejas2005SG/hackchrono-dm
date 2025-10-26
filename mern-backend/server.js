336efc2c7d66eaffb88492e2b8e071851cc88175
Tejas2005SG <tbhangale9@gmail.com>
2025-10-26 20:35:12 +0530

    deploy

--- mern-backend/server.js
+++ mern-backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import fetch from 'node-fetch'
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import DisasterDataProcessor from './csvProcessor.js';
+import DisasterDataProcessor from './csvprocessor.js';
 const app = express()
 const PORT = process.env.PORT || 3001
 import dotenv from 'dotenv'
